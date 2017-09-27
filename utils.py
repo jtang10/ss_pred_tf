02021af1f2b47c0e1cbdf9b5d54916429a1b7aa1
jtang10 <tangjingning@gmail.com>
2017-09-27 12:51:24 -0500

    add one seq validation, need to fix batch_norm

--- utils.py
+++ utils.py
@@ -1,55 +1,36 @@
 from __future__ import print_function
 from sys import getsizeof
+from random import shuffle
+import time
+import math
 import numpy as np
-import tensorflow as tf
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
 
-trainList_addr = './data/trainList'
-validList_addr = './data/validList'
-testList_addr = './data/testList'
+relative_path = './data/SetOf7604Proteins/'
+trainList_addr = relative_path + 'trainList'
+validList_addr = relative_path + 'validList'
+testList_addr = relative_path + 'testList'
 
 def read_list(filename):
     """Return a list of protein name
-
-    Args:
-        filename: the path of train, validation or test file.
-
-    Returns:
-        A list of proteins' names specified in each file.
     """
-    proteins_name = []
-    proteins_length = []
+    proteins_names = []
     with open(filename) as f:
         for line in f:
             protein_name = line.rstrip('\n')
-            prot_addr = './data/66FEAT/' + protein_name + '.66feat'
-            protein_length = 0
-            with open(prot_addr) as fs:
-                for i, l in enumerate(fs):
-                    pass
-            protein_length = i + 1
-            proteins_name.append(protein_name)
-            proteins_length.append(protein_length)
-    # print("List length is {}".format(len(ls)))
+            proteins_names.append(protein_name)
+    return proteins_names
 
-    return proteins_name, proteins_length
+def read_protein(prot_name, relative_path, expand_dims=False):
+    """Given a protein name, return a matrix of its features, corresponding
+       second structure information, sequence length and mask.
 
-def read_protein(prot_name, test=False):
-    """Given a protein name, return a matrix of its features and corresponding
-       second structure information
-
-    Args:
-        prot_name: The protein name provided by list of proteins from 
-            read_list(). 
-
-    Returns:
-        The numpy array of residue features and secondary structure label.
+    This is a sub-function of reading entire list of data and can also be
+    used to read individual protein.
     """
     ss = ['L', 'B', 'E', 'G', 'I', 'H', 'S', 'T']
     dict_ss = {key: value for (key, value) in zip(ss, range(8))}
-    prot_addr = './data/66FEAT/' + prot_name + '.66feat'
-    ss_addr = './data/Angles/' + prot_name + '.ang'
+    prot_addr = relative_path + '66FEAT/' + prot_name + '.66feat'
+    ss_addr = relative_path + 'Angles/' + prot_name + '.ang'
     prot_mat = np.loadtxt(prot_addr)
 
     ss_mat = []
@@ -58,28 +39,25 @@ def read_protein(prot_name, test=False):
         for i, line in enumerate(f):
             line = line.split('\t')
             if line[0] == '0':
-                if(test):
-                    ss_mat.append(line[3])
-                else:
-                    ss_mat.append(dict_ss[line[3]])
+                ss_mat.append(dict_ss[line[3]])
 
     ss_mat = np.array(ss_mat).transpose()
-    return prot_mat, ss_mat
+    seq_len = ss_mat.shape[0]
+    mask = np.ones((seq_len,), dtype=np.float32)
 
-def generate_batch(prot_list, prot_len_list, max_seq_length=300, batch_size=4):
+    if expand_dims:
+        prot_mat = np.expand_dims(prot_mat, axis=0)
+        ss_mat = np.expand_dims(ss_mat, axis=0)
+        seq_len = np.array([seq_len])
+        mask = np.expand_dims(mask, axis=0)
+
+    return prot_mat, ss_mat, seq_len, mask
+
+
+def generate_batch(prot_list, relative_path, max_seq_length=300, batch_size=4):
     """Given the list of protein name and length, generate a batch of feature 
        matrix and corresponding secondary structure label. Zero pad or cutoff 
        feature matrix based on max_seq_length.
-
-    Args:
-        prot_list: list of protein names provided by read_list() function.
-        prot_len_list: list of protein lengths in the same order of prot_list.
-        max_seq_length: cutoff length for protein sequences.
-        batch_size:
-
-    Returns:
-        a generator that generates zero-padded/cutoff residue feature matrix, 
-        secondary sturcture labels and a list of actual length for each protein.
     """
     while True:
         num_list = len(prot_list)
@@ -90,91 +68,58 @@ def generate_batch(prot_list, prot_len_list, max_seq_length=300, batch_size=4):
                                   dtype=np.float32)
         ss_labels_batch = np.zeros((batch_size, max_seq_length),
                                    dtype=np.int32)
-	mask_batch = np.zeros((batch_size, max_seq_length),
-			      dtype=np.int32)
+        mask_batch = np.zeros((batch_size, max_seq_length), dtype=np.int32)
         batch_seq_len = []
 
         for i, j in enumerate(batch_idx):
             protein_name = prot_list[j]
-            protein_features, ss_labels = read_protein(protein_name)
-            min_idx = min(max_seq_length, protein_features.shape[0])
+            protein_features, ss_labels, seq_len, mask = read_protein(protein_name, relative_path)
+            min_idx = min(max_seq_length, seq_len)
             proteins_batch[i, :min_idx, :] = protein_features[:min_idx, :]
             ss_labels_batch[i, :min_idx] = ss_labels[:min_idx]
-	    mask_batch[i, :min_idx] = 1
-            # batch_seq_len.append(prot_len_list[j])
+            mask_batch[i, :min_idx] = mask[:min_idx]
             batch_seq_len.append(min_idx)
 
         batch_seq_len = np.asarray(batch_seq_len, dtype=np.int32)
         yield proteins_batch, ss_labels_batch, batch_seq_len, mask_batch
 
-def read_data(prot_list, prot_len_list, max_seq_length=300):
+def read_data(prot_list, relative_path, max_seq_length=300):
     """Given lists of training, validation and test datasets, return the 
        ndarrays of all data, labels and lengths.
     """
     num_list = len(prot_list)
     proteins_all = np.zeros((num_list, max_seq_length, 66), dtype=np.float32)
     ss_labels_all = np.zeros((num_list, max_seq_length), dtype=np.int32)
-    mask_all = np.zeros((num_list, max_seq_length),
-			  dtype=np.int32)
+    mask_all = np.zeros((num_list, max_seq_length), dtype=np.float32)
     seq_lens_all = []
 
     for i, protein_name in enumerate(prot_list):
-        protein_features, ss_labels = read_protein(protein_name)
-        min_idx = min(max_seq_length, protein_features.shape[0])
+        protein_features, ss_labels, seq_len, mask = read_protein(protein_name, relative_path)
+        min_idx = min(max_seq_length, seq_len)
         proteins_all[i, :min_idx, :] = protein_features[:min_idx, :]
         ss_labels_all[i, :min_idx] = ss_labels[:min_idx]
-        mask_all[i, :min_idx] = 1
+        mask_all[i, :min_idx] = mask[:min_idx]
         seq_lens_all.append(min_idx)
     seq_lens_all = np.asarray(seq_lens_all, dtype=np.int32)
 
     return proteins_all, ss_labels_all, seq_lens_all, mask_all
 
-
-# Define test functions for some data preprocessing functions
-def test_read_protein():
-    test_protein = trainList[2][0]
-    print("Test protein name is {}".format(test_protein))
-    prot, ss = read_protein(test_protein, test=True)
-    print("Protein feature dimension is {}".format(prot.shape))
-    print("Second structure information dimension is {}".format(ss.shape))
-    print(ss)
-
-def test_generate_batch():
-    generator = generate_batch(trainList, train_len_list)
-    prot, ss_label, batch_seq_len = generator.next()
-
-    print(batch_seq_len)
-    print(prot.shape)
-    print(ss_label.shape)
-    print('_'*100)
-    # print(ss_label[1,:])
-
-def plot_length_distribution():
-    print("There are {} proteins in the training set" \
-          .format(len(train_len_list)))
-    # print(max(trainList, key=lambda item:item[1]))
-    count = 0
-    for i in train_len_list:
-        if i >= 400:
-            count += 1
-    print(count)
-    plt.hist(train_len_list, bins=50)
-    plt.grid(True)
-    plt.show()
-
 if __name__ == '__main__':
-    trainList, train_len_list = read_list(trainList_addr)
-    validList, valid_len_list = read_list(validList_addr)
-    testList, _ = read_list(testList_addr)
+    trainList = read_list(trainList_addr)
+    validList = read_list(validList_addr)
+    testList = read_list(testList_addr)
+    # print(len(validList))
+    # print(validList)
+    features, labels, seq_len, mask = read_protein(validList[0], elative_path, True)
+    print(features.shape)
+    print(labels.shape)
+    print(type(seq_len))
+    print(seq_len.shape)
+    print(mask.shape)
 
-    # test_read_protein()
-    # plot_length_distribution()
-    # test_generate_batch()
+    # proteins_all, ss_labels_all, seq_lens_all, mask_all = read_data(validList)
+    # print(proteins_all.shape)
+    # print(ss_labels_all.shape)
+    # print(seq_lens_all)
+    # print(mask_all.shape)
 
-    # generator = generate_batch(trainList, train_len_list)
-    # print(generator.next()[0].shape)
-    # print(generator.next()[1].shape)
-    # print(generator.next()[2])
-
-    a, b, c = read_data(validList, valid_len_list)
-    print(a.shape, b.shape, c.shape)
